75c4d9f9b39be8d133f6a0bae3396a1abd40b074
J-i-b-r-a-n <jibranjibby2@gmail.com>
2021-10-01 15:28:00 +0000

    Remove unwanted files

--- hashmifc/settings.py
+++ hashmifc/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$db!f=i!r&p10e2bd2-5)39ubz)3d*il(oz-m$^00=_vmpt=%1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hashmiclub.pythonanywhere.com']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
